eab564a62747c2ab9dd0fefc0e2410d354aee4e0
Vinetos <Vinetosdev@gmail.com>
2021-05-23 00:48:24 +0200

    feat: Reformat and add first form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,51 +2,51 @@
   <section>
     <Navbar/>
     <div class="container" style="margin-top: 3rem;">
-        <div class="columns is-vcentered">
-            <div class="column is-6">
+      <div class="columns is-vcentered">
+        <div class="column is-6">
 
-                <h1 class="title is-title is-size-1 has-text-weight-bold">Simplifiez vos
-                  démarches administratives en deux clics grâce à EZ-Tax</h1>
+          <h1 class="title is-title is-size-1 has-text-weight-bold">Simplifiez vos
+            démarches administratives en deux clics grâce à EZ-Tax</h1>
 
-                <p class="subtitle is-4 has-margin-top-20 is-family-primary">Inscrivez
-                  vous gratuitement dès maintenant.</p>
+          <p class="subtitle is-4 has-margin-top-20 is-family-primary">Inscrivez
+            vous gratuitement dès maintenant.</p>
 
-                <form method="get" action=".....">
-                    <div class="field has-addons has-margin-top-30">
-                        <div class="control">
-                            <input class="input is-medium" type="email"
-                                   name="email" placeholder="Your email">
-                        </div>
-                        <div class="control">
-                            <button type="submit" class="button is-primary is-medium">
-                                    Commencer !
-                                </button>
-                        </div>
-                    </div>
-                </form>
+          <form method="get" action=".....">
+            <div class="field has-addons has-margin-top-30">
+              <div class="control">
+                <input class="input is-medium" type="email"
+                       name="email" placeholder="Your email">
+              </div>
+              <div class="control">
+                <button type="submit" class="button is-primary is-medium">
+                  Commencer !
+                </button>
+              </div>
             </div>
-            <div class="column is-5 has-text-centered">
-                <figure class="image" style="width : 50rem;height : auto;">
-                    <img src="../assets/img3.png" alt="Pickle">
-                </figure>
-            </div>
+          </form>
         </div>
+        <div class="column is-5 has-text-centered">
+          <figure class="image" style="width : 50rem;height : auto;">
+            <img src="../assets/img3.png" alt="Pickle">
+          </figure>
+        </div>
+      </div>
     </div>
 
-    <section id="features" class="hero has-background-white-ter" style="margin-top: 5rem;" >
-    <div class="column is-10 is-offset-1 ">
+    <section id="features" class="hero has-background-white-ter" style="margin-top: 5rem;">
+      <div class="column is-10 is-offset-1 ">
 
         <div class="box">
-            <div class="columns">
-              <figure class="image " style="width : 25rem;height : auto;" >
-                <img src="../assets/img1.png" alt="get analytics and optimize your  performance">
-              </figure>
-              <div class="column">
-                <div class="title">
-                  <h2 class="has-text-black is-family-secondary has-text-weight-bold">
-                    Problématique ?</h2>
-                </div>
-                  <span class="is-size-5"> Lorsque nous rentrons dans la vingtaine
+          <div class="columns">
+            <figure class="image " style="width : 25rem;height : auto;">
+              <img src="../assets/img1.png" alt="get analytics and optimize your  performance">
+            </figure>
+            <div class="column">
+              <div class="title">
+                <h2 class="has-text-black is-family-secondary has-text-weight-bold">
+                  Problématique ?</h2>
+              </div>
+              <span class="is-size-5"> Lorsque nous rentrons dans la vingtaine
                     nous sommes confrontésà un problème de taille en France :
                      l'administratif. En effet rien ne nous prépare réellement à
                     devoir tout préparer et tout régler par nous même, trouver des
@@ -58,17 +58,17 @@
                     intéressent vraiment. Et bien, c'est à ce moment qu'intervient
                     EZ-Tax ! Notre slogan est "l'administration française n'a jamais été
                     aussi rapide."</span>
-              </div>
             </div>
+          </div>
         </div>
         <div class="box">
-            <div class="columns">
-              <div class="column">
-                <div class="title">
-                  <h2 class="has-text-black is-family-secondary has-text-weight-bold">
-                    La solution !</h2>
-                </div>
-                  <span class="is-size-5"> EZ-tax propose une
+          <div class="columns">
+            <div class="column">
+              <div class="title">
+                <h2 class="has-text-black is-family-secondary has-text-weight-bold">
+                  La solution !</h2>
+              </div>
+              <span class="is-size-5"> EZ-tax propose une
                     solution innovante à tout vos problèmes
                     d'administration ! Finies les heures interminables à
                     chercher un document, il sera directement inclus dans un
@@ -78,16 +78,14 @@
                     dont vous pourriez avoir besoin pour n'importe quel cas :
                     déclaration d'impôts, achat de véhicules, aides , demande de documents
                     officiels, nous pouvons même dans certains cas le faire pour vous.</span>
-              </div>
-              <figure class="image " style="width : 18rem;height : auto;">
-                <img src="../assets/img2.png" alt="get analytics and optimize your  performance">
-              </figure>
             </div>
+            <figure class="image " style="width : 18rem;height : auto;">
+              <img src="../assets/img2.png" alt="get analytics and optimize your  performance">
+            </figure>
           </div>
-
-    </div>
+        </div>
+      </div>
     </section>
-
     <Footer/>
   </section>
 </template>
@@ -107,7 +105,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.box{
+.box {
   margin-top: 4rem;
 }
 </style>
--- src/views/connexion.vue
+++ src/views/connexion.vue
@@ -1,53 +1,64 @@
 <template>
 
-<section >
+  <section>
     <Navbar/>
 
-            <div class="container has-text-centered" style="margin-top:10%">
-                <div class="column is-4 is-offset-4 ">
-                    <h3 class="title has-text-black">Connexion</h3>
+    <div class="container has-text-centered" style="margin-top:10%">
+      <div class="column is-4 is-offset-4 ">
+        <h3 class="title has-text-black">Connexion</h3>
 
-                    <div class="box">
+        <div class="box">
 
-                        <form>
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-large" type="email"
-                                           placeholder="Email" autofocus="">
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-large"
-                                           type="password" placeholder="mot de passe">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="checkbox">
-                                <input type="checkbox">
-                                Se souvenir de moi
-                                </label>
-                            </div>
-                            <button class="button is-block is-info is-large is-fullwidth">
-                              Se connecter </button>
-                        </form>
-                    </div>
-
-                        <p class="has-text-grey">
-                        <b-navbar-item :to="{ path: '/enregistrer' }"
-                                       tag="router-link">
-                          S'enregistrer</b-navbar-item > &nbsp;·&nbsp;
-                        </p>
-
-                </div>
+          <form onsubmit="return false">
+            <div class="field">
+              <b-field label="Email"
+                       :type="err ? 'is-danger' : ''"
+                       placeholder="Jean@dupond.fr">
+                <b-input type="email"
+                         v-bind="email"
+                         maxlength="30">
+                </b-input>
+              </b-field>
             </div>
 
-    </section>
+            <div class="field">
+              <b-field label="Mot de passe"
+                       :type="err ? 'is-danger' : ''"
+                       placeholder="">
+                <b-input type="password"
+                         v-bind="passw"
+                         maxlength="32">
+                </b-input>
+              </b-field>
+            </div>
+            <div class="field">
+              <label class="checkbox">
+                <input type="checkbox">
+                Se souvenir de moi
+              </label>
+            </div>
+            <button class="button is-block is-info is-large is-fullwidth" v-on:click="login()">
+              Se connecter
+            </button>
+          </form>
+        </div>
+
+        <p class="has-text-grey">
+          <b-navbar-item :to="{ path: '/enregistrer' }"
+                         tag="router-link">
+            S'enregistrer
+          </b-navbar-item> &nbsp;·&nbsp;
+        </p>
+
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from 'axios';
 import Navbar from '../components/Navbar.vue';
 
 export default {
@@ -55,14 +66,60 @@ export default {
   components: {
     Navbar,
   },
+  data() {
+    return {
+      err: undefined,
+      email: undefined,
+      passw: undefined,
+    };
+  },
+  methods: {
+    login() {
+      console.log('Chien');
+      // Fetches posts when the component is created.
+      axios.post('http://vps.vinetos.fr:9090/login',
+        {
+          email: this.email,
+          passwd: this.passw,
+        })
+        .then((response) => {
+          // JSON responses are automatically parsed.
+          console.log(response.data);
+          this.err = true;
+          this.$buefy.dialog.alert({
+            title: 'OK',
+            message: 'Something\'s not good but I have a custom <b>icon</b> and <b>type</b>',
+            type: 'is-danger',
+            hasIcon: true,
+            icon: 'times-circle',
+            iconPack: 'fa',
+            ariaRole: 'alertdialog',
+            ariaModal: true,
+          });
+        })
+        .catch((e) => {
+          this.err = true;
+          this.$buefy.dialog.alert({
+            title: 'Error',
+            message: 'Something\'s not good but I have a custom <b>icon</b> and <b>type</b>',
+            type: 'is-danger',
+            hasIcon: true,
+            icon: 'times-circle',
+            iconPack: 'fa',
+            ariaRole: 'alertdialog',
+            ariaModal: true,
+          });
+        });
+    },
+  },
 };
 
 </script>
 
 <style scoped lang="scss">
 
-.hero{
-    background-color: blue;
+.hero {
+  background-color: blue;
 }
 
 </style>
